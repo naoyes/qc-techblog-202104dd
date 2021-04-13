8dfcb2e8eb3c002d8ee4ca7aa14981dc607e1271
Naoyasu MANABE <n-manabe@quartetcom.co.jp>
2021-04-13 17:35:37 +0900

    VPC および サブネットの配置（未実行）

--- lib/qc-techblog-202104dd-stack.ts
+++ lib/qc-techblog-202104dd-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class QcTechblog202104DdStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      cidr: '10.11.0.0/16',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'rds-client',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 28,
+          name: 'rds',
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+      ],
+    });
   }
 }
